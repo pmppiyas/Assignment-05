294f0d7c3fa54b77d034c297339941dabbf8fff3
Prince Mahmud Piyas <princemahmudpiyas@gmail.com>
2024-09-23 18:36:58 +0600

    button workable

--- js/utilities.js
+++ js/utilities.js
@@ -1,4 +1,16 @@
 // BlogPage Onclick
 document.getElementById('blog-btn').addEventListener('click', function () {
-  window.location.href = '../faq.html';
+  window.location.href = './faq.html';
 });
+
+// Section Declear
+const historySection = document.getElementById('history-section');
+const donationSection = document.getElementById('donate-section');
+
+// History Button
+document
+  .getElementById('history-section-btn')
+  .addEventListener('click', function () {
+    historySection.classList.remove('hidden');
+    donationSection.classList.add('hidden');
+  });
